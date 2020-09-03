c23b289accdc87f194f3372f246c75ad533d4a31
Ghulam <ghulamjillani216@gmail.com>
2020-09-04 02:12:58 +0500

    update app.js
    Simple update for git practice

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br/> Update by Jillani
+          <br/> A simple page made on reactjs
         </p>
         <a
           className="App-link"
